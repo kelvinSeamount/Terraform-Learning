30a43eb8635d0097f35970596d7ba1c04855bfb7
kelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-09-22 10:32:29 +0200

    modify instance ouput based on IP

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "publicip" {
-   value = aws_instance.my_instance.public_ip
+   value = [ for  instance in aws_instance.my_instance : instance.public_ip ] # Output the public IPs of all instances
 }
