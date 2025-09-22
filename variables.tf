42b4048014d444420a2a85f03a25c95e85f8f921
kelvinSeamount <kelvinnwachukwu89@gmail.com>
2025-09-22 09:18:53 +0200

    create variable file

---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "region" {
+  
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "vpc_name" {
+  
+}
+
+variable "cidr_block_subnet" {
+  
+}
+
+variable "azone" {
+  
+}
+
+variable "subnet_name" {
+  
+}
+
+variable "sg_name" {
+  
+}
+
+variable "route_table_name" {
+  
+}
+
+variable "igw_name" {
+  
+}
+
+variable "ami_id" {
+  
+}
+
+variable "key_name" {
+  
+}
+
+variable "itype" {
+  
+}
+
+variable "instance_name" {
+  
+}
+
+variable "volume_size" {
+  
+}
+
+variable "volume_type" {
+  
+}
